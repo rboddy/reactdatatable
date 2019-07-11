00ccca38105ed6dd249355e407ea483c1a87a0dd
rboddy <avidryanb@gmail.com>
2019-07-10 22:07:58 -0400

    Switching to arrow key and button logic

--- client/src/components/layout/Table.js
+++ client/src/components/layout/Table.js
@@ -13,7 +13,7 @@ const Table = () => {
       Category_1: ""
     }
   ]);
-  const [upperLimit, setUpperLimit] = useState(15);
+  const [upperLimit, setUpperLimit] = useState(6);
   const [lowerLimit, setLowerLimit] = useState(0);
   const [isFetching, setIsFetching] = useState(false);
 
@@ -26,35 +26,24 @@ const Table = () => {
 
     fetchData();
 
-    // add event listeners and functions to the different scroll directions
-    function handleScrollDown() {
-      if (
-        window.innerHeight + window.scrollY !==
-        document.documentElement.offsetHeight
-      )
-        return;
-      console.log("Time to fetch more data!");
-      setLowerLimit(lowerLimit + 15);
-      setUpperLimit(upperLimit + 15);
-      setIsFetching(true);
-    }
-    window.addEventListener("scroll", handleScrollDown);
-    const handleScrollUp = function(e) {
-      if (e.deltaY < 1 && lowerLimit !== 0) {
-        console.log(lowerLimit);
-        if (document.documentElement.scrollTop !== 0) return;
-        console.log("Time to go back");
-        setLowerLimit(lowerLimit - 15);
-        setUpperLimit(upperLimit - 15);
-        setIsFetching(true);
+    //setup the arrow keys
+    document.onkeydown = function(e) {
+      switch (e.keyCode) {
+        case 38:
+          if (lowerLimit !== 0) {
+            console.log("Time to go back");
+            setLowerLimit(lowerLimit - 15);
+            setUpperLimit(upperLimit - 15);
+            setIsFetching(true);
+          }
+          break;
+        case 40:
+          console.log("Time to fetch more data!");
+          setLowerLimit(lowerLimit + 15);
+          setUpperLimit(upperLimit + 15);
+          setIsFetching(true);
+          break;
       }
-    };
-    window.addEventListener("wheel", handleScrollUp);
-
-    // Remove the event listeners so that they aren't commited multiple times when the effect is reused
-    return () => {
-      window.removeEventListener("scroll", handleScrollDown);
-      window.removeEventListener("wheel", handleScrollUp);
     };
   }, [lowerLimit, upperLimit]);
 
@@ -68,41 +57,68 @@ const Table = () => {
       };
       fetchData();
       setIsFetching(false);
-      window.scrollTo(0, 0);
     }
 
     fetchMoreData();
   }, [isFetching, upperLimit, lowerLimit]);
 
+  function dataUp() {
+    if (lowerLimit !== 0) {
+      console.log("Time to go back");
+      setLowerLimit(lowerLimit - 15);
+      setUpperLimit(upperLimit - 15);
+      setIsFetching(true);
+    }
+  }
+  function dataDown() {
+    console.log("Time to fetch more data!");
+    setLowerLimit(lowerLimit + 15);
+    setUpperLimit(upperLimit + 15);
+    setIsFetching(true);
+  }
+
   /* Generate a grid that uses the limits set in state to slice a chunk of the whole dataset to display. 
      Passed the data to a lower level component for the rows themselves with props because I thought it was more manageable.*/
   return (
-    <div className="dataTable" id="dataTable">
-      <div className="dataTableHeader">ID</div>
-      <div className="dataTableHeader">Content</div>
-      <div className="dataTableHeader">Time</div>
-      <div className="dataTableHeader">Categories</div>
-      {data.slice(lowerLimit, upperLimit).map(item => (
-        <TableRow
-          key={uuid.v4()}
-          id={item.id}
-          content={item.Content}
-          time={item.Time}
-          categories={[
-            item.Category_1,
-            item.Category_2,
-            item.Category_3,
-            item.Category_4,
-            item.Category_5,
-            item.Category_6,
-            item.Category_7,
-            item.Category_8,
-            item.Category_9,
-            item.Category_10,
-            item.Category_11
-          ]}
-        />
-      ))}
+    <div>
+      <div className="instructions">
+        <p>Use the up and down arrow keys to load next or previous rows.</p>
+        <p>Alternatively, use these buttons</p>
+        <button className="removeBtn" onClick={dataUp}>
+          <i className="fas fa-arrow-circle-up fa-3x" />
+        </button>
+        <button className="removeBtn" onClick={dataDown}>
+          <i className="fas fa-arrow-circle-down fa-3x" />
+        </button>
+      </div>
+
+      <div className="dataTable" id="dataTable">
+        <div className="dataTableHeader">ID</div>
+        <div className="dataTableHeader">Content</div>
+        <div className="dataTableHeader">Time</div>
+        <div className="dataTableHeader">Categories</div>
+        {data.slice(lowerLimit, upperLimit).map(item => (
+          <TableRow
+            key={uuid.v4()}
+            id={item.id}
+            content={item.Content}
+            time={item.Time}
+            categories={[
+              item.Category_1,
+              item.Category_2,
+              item.Category_3,
+              item.Category_4,
+              item.Category_5,
+              item.Category_6,
+              item.Category_7,
+              item.Category_8,
+              item.Category_9,
+              item.Category_10,
+              item.Category_11
+            ]}
+          />
+        ))}
+      </div>
     </div>
   );
 };
